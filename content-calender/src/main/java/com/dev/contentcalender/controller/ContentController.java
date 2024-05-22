7bec64fe52bb3f7fac42ba974559a292b2a3dd66
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-22 17:10:59 +0100

    Add a dependency injection of ContentCollectionRepository and inject it using the constructor

--- content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
@@ -1,10 +1,15 @@
 package com.dev.contentcalender.controller;
 
+import com.dev.contentcalender.repository.ContentCollectionRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
 
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
 }
