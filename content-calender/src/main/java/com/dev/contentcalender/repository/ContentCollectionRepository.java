1ab2ba4b155a10bd860f2686c4414f797752f5e9
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-22 17:20:08 +0100

    create method to be called after the dependency injection using PostConstructor annotation

--- content-calender/src/main/java/com/dev/contentcalender/repository/ContentCollectionRepository.java
+++ content-calender/src/main/java/com/dev/contentcalender/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package com.dev.contentcalender.repository;
 
 import com.dev.contentcalender.model.Content;
+import com.dev.contentcalender.model.Status;
+import com.dev.contentcalender.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +23,18 @@ public class ContentCollectionRepository {
     }
     public Optional<Content> findById(Integer id){
         return content.stream().filter(c -> c.getId().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init(){
+        Content c=new Content(1,
+                "My first blog post",
+                "my first desc of blog",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+        content.add(c);
     }
 }
