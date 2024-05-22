f502a112bf8459f5c128425dc2622e235a7772b5
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-22 17:13:41 +0100

    create findAll method with getMapping annotation

--- content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
@@ -1,8 +1,12 @@
 package com.dev.contentcalender.controller;
 
+import com.dev.contentcalender.model.Content;
 import com.dev.contentcalender.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
@@ -11,5 +15,9 @@ public class ContentController {
 
     public ContentController(ContentCollectionRepository repository) {
         this.repository = repository;
+    }
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
     }
 }
