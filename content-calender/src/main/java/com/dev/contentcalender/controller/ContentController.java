1b73726dd518a4b0d051f44aa7402c95365bab3a
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-22 16:59:21 +0100

    add annotations to controller

--- content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
@@ -1,2 +1,10 @@
-package com.dev.contentcalender.controller;public class ContentController {
+package com.dev.contentcalender.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
 }
