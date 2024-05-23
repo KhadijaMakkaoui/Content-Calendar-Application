2eed2fcd770627dbe8c6f30a80611a275ba3c6c4
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-23 17:45:55 +0100

    add CrossOrigin annotation to allow connection with front project

--- content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/com/dev/contentcalender/controller/ContentController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
     private final ContentCollectionRepository repository;
 
