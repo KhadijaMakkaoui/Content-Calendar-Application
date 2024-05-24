336c8cfaf2baec8747480ead6c989a5269c78995
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-05-24 09:19:39 +0100

    annotate title attribut with NotBlank annotation The annotated element must not be null and must contain at least one non-whitespace character

--- content-calender/src/main/java/com/dev/contentcalender/model/Content.java
+++ content-calender/src/main/java/com/dev/contentcalender/model/Content.java
@@ -1,9 +1,12 @@
 package com.dev.contentcalender.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public class Content {
     Integer id;
+    @NotBlank
     String title;
     String desc;
     Status status;
